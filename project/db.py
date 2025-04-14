8cf89229c606bed8ce2c946489303983b98754e9
dewanshkumar123 <dewansh.kumar@iitgn.ac.in>
2025-04-14 12:31:12 +0530

    CIMS support added

--- project/db.py
+++ project/db.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_connection():
+def get_connection(cims = True):
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASS'),
-        database=os.getenv('DB_DATABASE')
+        database=os.getenv('DB_DATABASE'+cims*'CIMS')
     )
