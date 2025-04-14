b45f1a525420ffabdf762eb6c62060f26d809592
Your Name <youremail@yourdomain.com>
2025-04-14 06:48:49 +0000

    Database added

--- project/db.py
+++ project/db.py
@@ -9,4 +9,5 @@ def get_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASS'),
+        database=os.getenv('DB_DATABASE')
     )
